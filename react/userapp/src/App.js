e255f8946651e5fddc254d94abfcfb1b1924c556
RealBigHummus <bighummus0987@gmail.com>
2022-04-03 19:10:52 +0300

    idk

---
+++ react/userapp/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import User from './User.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <User />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/userapp/src/User.js
@@ -0,0 +1,9 @@
+const User = () => {
+return (
+    <div>
+        <h1> User Compnent  </h1>
+            </div>
+)
+
+}
+export default User
